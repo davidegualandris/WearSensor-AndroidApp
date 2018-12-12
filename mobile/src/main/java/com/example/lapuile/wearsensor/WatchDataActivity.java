9332d435d7a25ea74117775f8f68cf881a6510d3
Gianmaria Balducci <g.balducci1@campus.unimib.it>
2018-12-12 18:44:31 +0100

    jntrkernp

---
+++ mobile/src/main/java/com/example/lapuile/wearsensor/WatchDataActivity.java
@@ -0,0 +1,177 @@
+package com.example.lapuile.wearsensor;
+
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.renderscript.Element;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptionsExtension;
+import com.google.android.gms.fitness.request.DataReadRequest;
+import com.google.android.gms.fitness.result.DataReadResponse;
+import com.google.android.gms.tasks.Task;
+
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSource;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.data.Value;
+import com.google.android.gms.fitness.request.DataSourcesRequest;
+import com.google.android.gms.fitness.request.OnDataPointListener;
+import com.google.android.gms.fitness.request.SensorRequest;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+
+public class WatchDataActivity extends AppCompatActivity {
+
+    public static final String TAG = "BasicSensorsApi";
+
+    private OnDataPointListener mListener;
+
+    private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 34;
+
+    private float[] watchValue;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_watch_data);
+
+
+
+
+
+
+
+    }
+
+
+    private void registerFitnessDataListener(DataSource dataSource, DataType dataType) {
+        // [START register_data_listener]
+        mListener =
+                new OnDataPointListener() {
+                    @Override
+                    public void onDataPoint(DataPoint dataPoint) {
+                        for (Field field : dataPoint.getDataType().getFields()) {
+                            Value val = dataPoint.getValue(field);
+                            Log.i(TAG, "Detected DataPoint field: " + field.getName());
+                            Log.i(TAG, "Detected DataPoint value: " + val);
+                        }
+                    }
+                };
+
+        Fitness.getSensorsClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .add(
+                        new SensorRequest.Builder()
+                                .setDataSource(dataSource) // Optional but recommended for custom data sets.
+                                .setDataType(dataType) // Can't be omitted.
+                                .setSamplingRate(10, TimeUnit.SECONDS)
+                                .build(),
+                        mListener)
+                .addOnCompleteListener(
+                        new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Log.i(TAG, "Listener registered!");
+                                } else {
+                                    Log.e(TAG, "Listener not registered.", task.getException());
+                                }
+                            }
+                        });
+
+
+    }
+
+
+    private void unregisterFitnessDataListener() {
+        if (mListener == null) {
+            // This code only activates one listener at a time.  If there's no listener, there's
+            // nothing to unregister.
+            return;
+        }
+
+        // [START unregister_data_listener]
+        // Waiting isn't actually necessary as the unregister call will complete regardless,
+        // even if called from within onStop, but a callback can still be added in order to
+        // inspect the results.
+        Fitness.getSensorsClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .remove(mListener)
+                .addOnCompleteListener(
+                        new OnCompleteListener<Boolean>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Boolean> task) {
+                                if (task.isSuccessful() && task.getResult()) {
+                                    Log.i(TAG, "Listener was removed!");
+                                } else {
+                                    Log.i(TAG, "Listener was not removed.");
+                                }
+                            }
+                        });
+        // [END unregister_data_listener]
+    }
+
+    public void playSensorDataWear(View view) {
+
+        Fitness.getSensorsClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .findDataSources(
+                        new DataSourcesRequest.Builder()
+                                .setDataTypes(DataType.TYPE_HEART_RATE_BPM)
+                                .setDataSourceTypes(DataSource.TYPE_RAW)
+                                .build())
+                .addOnSuccessListener(
+                        new OnSuccessListener<List<DataSource>>() {
+                            @Override
+                            public void onSuccess(List<DataSource> dataSources) {
+                                for (DataSource dataSource : dataSources) {
+                                    Log.i(TAG, "Data source found: " + dataSource.toString());
+                                    Log.i(TAG, "Data Source type: " + dataSource.getDataType().getName());
+
+                                    // Let's register a listener to receive Activity data!
+                                    if (dataSource.getDataType().equals(DataType.TYPE_HEART_RATE_BPM)
+                                            && mListener == null) {
+                                        Log.i(TAG, "Data source for LOCATION_SAMPLE found!  Registering.");
+                                        registerFitnessDataListener(dataSource, DataType.TYPE_HEART_RATE_BPM);
+                                    }
+                                }
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.e(TAG, "failed", e);
+                            }
+                        });
+
+    }
+
+    public void pauseSensorDataWear(View view) {
+
+        unregisterFitnessDataListener();
+
+
+    }
+}
+
+
+
+
