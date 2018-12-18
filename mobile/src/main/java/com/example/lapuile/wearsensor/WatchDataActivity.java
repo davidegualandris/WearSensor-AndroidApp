e502c40d3068704f1b04178de97e59d791635f60
root <g.balducci1@campus.unimib.it>
2018-12-18 12:42:56 +0100

    18 dic ù

--- mobile/src/main/java/com/example/lapuile/wearsensor/WatchDataActivity.java
+++ mobile/src/main/java/com/example/lapuile/wearsensor/WatchDataActivity.java
@@ -3,6 +3,7 @@ package com.example.lapuile.wearsensor;
 
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.renderscript.Element;
 import android.support.annotation.NonNull;
@@ -10,10 +11,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptionsExtension;
+import com.google.android.gms.fitness.data.BleDevice;
+import com.google.android.gms.fitness.request.BleScanCallback;
 import com.google.android.gms.fitness.request.DataReadRequest;
 import com.google.android.gms.fitness.result.DataReadResponse;
 import com.google.android.gms.tasks.Task;
@@ -33,6 +41,8 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -42,18 +52,81 @@ import java.util.concurrent.TimeUnit;
 public class WatchDataActivity extends AppCompatActivity {
 
     public static final String TAG = "BasicSensorsApi";
+    private static final int GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 34 ;
 
     private OnDataPointListener mListener;
+    ListView sensor_list;
 
-    private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 34;
 
     private float[] watchValue;
+
+    BleDevice deviceWear;
+
+    Field fieldWear;
+    Value valWear;
+
+
+
+    private GoogleSignInClient mGoogleSignInClient;
+    private OnDataPointListener myStepCountListener;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watch_data);
+
+        sensor_list = (findViewById(R.id.sensor_list));
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+
+        BleScanCallback bleScanCallbacks = new BleScanCallback() {
+            @Override
+            public void onDeviceFound(BleDevice device) {
+
+                Log.i(TAG, "Device Found");
+                deviceWear = device;
+
+
+            }
+            @Override
+            public void onScanStopped() {
+                Log.i(TAG, "Scan Stopped");
+
+            }
+        };
+
+        FitnessOptions fitnessOptions = FitnessOptions.builder()
+                .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+               // .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+                .build();
+
+        if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions)) {
+            GoogleSignIn.requestPermissions(
+                    this, // your activity
+                    GOOGLE_FIT_PERMISSIONS_REQUEST_CODE,
+                    GoogleSignIn.getLastSignedInAccount(this),
+                    fitnessOptions);
+        } else {
+            accessGoogleFit();
+        }
+
+
+
+        GoogleSignInAccount gsa = GoogleSignIn.getAccountForExtension(this, fitnessOptions);
+
+
+
+        Task<Void> response = Fitness.getBleClient(this, gsa)
+                .startBleScan(Arrays.asList(DataType.TYPE_STEP_COUNT_DELTA),
+                        1000, bleScanCallbacks);
+
 
 
 
@@ -63,8 +136,71 @@ public class WatchDataActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == GOOGLE_FIT_PERMISSIONS_REQUEST_CODE) {
+                accessGoogleFit();
+            }
+        }
+    }
+
+    private void accessGoogleFit() {
+
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        long endTime = cal.getTimeInMillis();
+        cal.add(Calendar.YEAR, -1);
+        long startTime = cal.getTimeInMillis();
+
+
+
+
+        Task<Void> response = Fitness.getBleClient(this,
+                GoogleSignIn.getLastSignedInAccount(this)).claimBleDevice(deviceWear);
+
+        Fitness.getSensorsClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .findDataSources(
+                        new DataSourcesRequest.Builder()
+                                .setDataTypes(DataType.TYPE_STEP_COUNT_DELTA)
+                                .setDataSourceTypes(DataSource.TYPE_DERIVED)
+                                .build())
+                .addOnSuccessListener(
+                        new OnSuccessListener<List<DataSource>>() {
+                            @Override
+                            public void onSuccess(List<DataSource> dataSources) {
+                                for (DataSource dataSource : dataSources) {
+                                    Log.i(TAG, "Data source found: " + dataSource.toString());
+                                    Log.i(TAG, "Data Source type: " + dataSource.getDataType().getName());
+
+
+
+                                    // Let's register a listener to receive Activity data!
+                                    if (dataSource.getDataType().equals(DataType.TYPE_STEP_COUNT_DELTA)
+                                            && mListener == null) {
+                                        Log.i(TAG, "Data source for LOCATION_SAMPLE found!  Registering.");
+                                        registerFitnessDataListener(dataSource, DataType.TYPE_STEP_COUNT_DELTA);
+                                        setDataView();
+                                    }
+                                }
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.e(TAG, "failed", e);
+                            }
+                        });
+
+
+    }
+
 
     private void registerFitnessDataListener(DataSource dataSource, DataType dataType) {
+
+
         // [START register_data_listener]
         mListener =
                 new OnDataPointListener() {
@@ -74,6 +210,11 @@ public class WatchDataActivity extends AppCompatActivity {
                             Value val = dataPoint.getValue(field);
                             Log.i(TAG, "Detected DataPoint field: " + field.getName());
                             Log.i(TAG, "Detected DataPoint value: " + val);
+                            fieldWear = field;
+                            valWear = val;
+
+
+
                         }
                     }
                 };
@@ -97,6 +238,22 @@ public class WatchDataActivity extends AppCompatActivity {
                                 }
                             }
                         });
+
+
+
+
+    }
+
+    private void setDataView(){
+
+        if(fieldWear != null && valWear != null) {
+            final ArrayList<String> listWear = new ArrayList<String>();
+            listWear.add(fieldWear.getName());
+            final ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                    (this, R.layout.my_layout, listWear);
+
+            sensor_list.setAdapter(adapter);
+        }
 
 
     }
@@ -129,44 +286,20 @@ public class WatchDataActivity extends AppCompatActivity {
         // [END unregister_data_listener]
     }
 
-    public void playSensorDataWear(View view) {
 
-        Fitness.getSensorsClient(this, GoogleSignIn.getLastSignedInAccount(this))
-                .findDataSources(
-                        new DataSourcesRequest.Builder()
-                                .setDataTypes(DataType.TYPE_HEART_RATE_BPM)
-                                .setDataSourceTypes(DataSource.TYPE_RAW)
-                                .build())
-                .addOnSuccessListener(
-                        new OnSuccessListener<List<DataSource>>() {
-                            @Override
-                            public void onSuccess(List<DataSource> dataSources) {
-                                for (DataSource dataSource : dataSources) {
-                                    Log.i(TAG, "Data source found: " + dataSource.toString());
-                                    Log.i(TAG, "Data Source type: " + dataSource.getDataType().getName());
 
-                                    // Let's register a listener to receive Activity data!
-                                    if (dataSource.getDataType().equals(DataType.TYPE_HEART_RATE_BPM)
-                                            && mListener == null) {
-                                        Log.i(TAG, "Data source for LOCATION_SAMPLE found!  Registering.");
-                                        registerFitnessDataListener(dataSource, DataType.TYPE_HEART_RATE_BPM);
-                                    }
-                                }
-                            }
-                        })
-                .addOnFailureListener(
-                        new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.e(TAG, "failed", e);
-                            }
-                        });
+
+
+        public void playSensorDataWear(View view) {
+            accessGoogleFit();
 
     }
 
     public void pauseSensorDataWear(View view) {
-
         unregisterFitnessDataListener();
+        Task<Void> response = Fitness.getBleClient(this,
+                GoogleSignIn.getLastSignedInAccount(this)).unclaimBleDevice(deviceWear);
+
 
 
     }
